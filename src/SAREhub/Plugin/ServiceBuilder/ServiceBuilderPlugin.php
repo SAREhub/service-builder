57698c860952f429695aa1c2b6b76888a59e51f9
jcala <j.cala@sarehub.com>
2018-05-24 16:43:43 +0200

    test

--- src/SAREhub/Plugin/ServiceBuilder/ServiceBuilderPlugin.php
+++ src/SAREhub/Plugin/ServiceBuilder/ServiceBuilderPlugin.php
@@ -44,6 +44,7 @@ class ServiceBuilderPlugin implements PluginInterface, EventSubscriberInterface,
 
     public function getCapabilities()
     {
+        var_dump("asdf");
         return [
             CommandProvider::class => CommandsProvider::class,
         ];
