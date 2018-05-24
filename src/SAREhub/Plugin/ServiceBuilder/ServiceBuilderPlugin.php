c5448d1c1e5d1c07a4d47549a819b0f8e7ceadfa
jcala <j.cala@sarehub.com>
2018-05-24 13:19:21 +0200

    plugin skeleton

---
+++ src/SAREhub/Plugin/ServiceBuilder/ServiceBuilderPlugin.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace SAREhub\Plugin\ServiceBuilder;
+
+use Composer\Composer;
+use Composer\IO\IOInterface;
+use Composer\Plugin\PluginInterface;
+
+class ServiceBuilderPlugin implements PluginInterface
+{
+    public function activate(Composer $composer, IOInterface $io)
+    {
+
+    }
+}
