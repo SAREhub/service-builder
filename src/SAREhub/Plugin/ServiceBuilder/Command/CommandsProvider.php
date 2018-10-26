dfb71b364afcd4713459c189b172c21b8e20797d
awasiak <a.wasiak@sare.pl>
2018-10-26 14:37:07 +0200

    remove invalid import in CommandsProvider

--- src/SAREhub/Plugin/ServiceBuilder/Command/CommandsProvider.php
+++ src/SAREhub/Plugin/ServiceBuilder/Command/CommandsProvider.php
@@ -4,7 +4,6 @@ namespace SAREhub\Plugin\ServiceBuilder\Command;
 
 
 use Composer\Plugin\Capability\CommandProvider;
-use SAREhub\Plugin\ServiceBuilder\Repository\RepositoryRegistry;
 
 class CommandsProvider implements CommandProvider
 {
